c862bdadbcb2295cf5757949e56320312c421896
minombreesjorge <escalonajorge73@gmail.com>
2025-01-24 22:56:34 -0400

    Agregados los componentes de alertas para finanzas y marketing

---
+++ src/components/Alerts/Alerts_panel_finanzas/AlertsPanelFinanzas.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+interface Alert {
+  type: 'success' | 'warning';
+  message: string;
+}
+
+const alerts = ref<Alert[]>([]);
+
+const fetchAlerts = async (): Promise<void> => {
+  alerts.value = [
+    {
+      type: 'success',
+      message: 'Los gastos superaron los ingresos en marzo.',
+    },
+    {
+      type: 'success',
+      message: 'Aumento del 15% en ingresos respecto al mes anterior.',
+    },
+    {
+      type: 'warning',
+      message: 'El flujo de caja ha sido negativo en los últimos 3 meses.',
+    },
+  ];
+};
+
+onMounted(() => {
+  fetchAlerts();
+});
+</script>
+
+<template>
+  <div class="alerts-panel">
+    <h2 class="alerts-title">Insights Financieros</h2>
+    <p>Resumen de puntos clave</p>
+    <div class="alerts-container">
+      <div
+        v-for="(alert, index) in alerts"
+        :key="index"
+        :class="['alert-card', alert.type]"
+      >
+        <span class="alert-symbol">~</span>
+        {{ alert.message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="./AlertsPanelFinanzas.css" scoped>
+
+</style>
---
+++ src/components/Alerts/Alerts_panel_marketing/AlertsPanelMarketing.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+
+interface Alert {
+  type: 'success' | 'warning' | 'info';
+  message: string;
+}
+
+const alerts = ref<Alert[]>([]);
+
+const fetchAlerts = async (): Promise<void> => {
+  alerts.value = [
+    {
+      type: 'success',
+      message: 'La campaña "Primavera" está teniendo un rendimiento excepcional',
+    },
+    {
+      type: 'warning',
+      message: 'La tasa de clics en la campaña de email ha disminuido',
+    },
+    {
+      type: 'info',
+      message: 'Recomendación: Aumentar el presupuesto para anuncios en LinkedIn',
+    },
+  ];
+};
+
+onMounted(() => {
+  fetchAlerts();
+});
+</script>
+
+<template>
+  <div class="alerts-panel">
+    <h2 class="alerts-title">Alertas y Recomendaciones</h2>
+    <div class="alerts-container">
+      <div
+        v-for="(alert, index) in alerts"
+        :key="index"
+        :class="['alert-card', alert.type]"
+      >
+        <span class="alert-symbol">~</span>
+        {{ alert.message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="./AlertsPanelMarketing.css" scoped ></style>
