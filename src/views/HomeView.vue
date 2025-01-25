299d0ae50279f9ca39ab505c9a84678313ce381d
EdgarUWU <edgaraponte398@gmail.com>
2025-01-24 23:06:32 -0400

    tarjeta de clasificaciones de alimentos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
+  <ratingcard/>
   <TarjetaCurso />
   <VCalendario />
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ratingcard from '../components/ratingcard/ratingcard.vue';
 import TarjetaCurso from '../components/tarjetacurso/tarjetacurso.vue'; 
 import VCalendario from '../components/VCalendario/VCalendario.vue'; 
 
@@ -16,6 +18,7 @@ export default defineComponent({
   components: {
     VCalendario,
     TarjetaCurso,
+    ratingcard
   },
 });
 </script>
