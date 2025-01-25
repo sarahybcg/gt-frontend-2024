a4649276b988759413d4d428c0b15f8221321716
unknown <rojashector540@gmail.com>
2025-01-24 20:19:22 -0400

    Realizacion de componentes widgets y grafico

---
+++ src/components/graphics/graphics.vue
@@ -0,0 +1,16 @@
+<template src="./graphics.html">
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './graphics.js';
+
+export default defineComponent({
+  name: 'GraphicsComponent',
+});
+</script>
+
+<style src="./graphics.css">
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <WidgetsComponent />
+    <GraphicsComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import WidgetsComponent from '@/components/widgets/widgets.vue'; // Importa el componente con el nuevo nombre
+import GraphicsComponent from '@/components/graphics/graphics.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
+    WidgetsComponent,
+    GraphicsComponent,
   },
 });
 </script>
