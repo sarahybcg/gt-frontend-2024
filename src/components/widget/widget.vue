8a14931c6cf30040b4ad37b8837b5d23028d3f52
Miriangel14 <miriangelpena6@gmail.com>
2025-01-27 12:04:20 -0400

    Componentes de los Widget

---
+++ src/components/widget/widget.vue
@@ -0,0 +1,15 @@
+<template src="./widget.html">
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ComponenteWidget',
+});
+</script>
+
+<style src="./widget.css">
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ComponenteWidget/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ComponenteWidget from '@/components/widget/widget.vue'; // Importa el componente con el nuevo nombre
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
+    ComponenteWidget,
   },
 });
 </script>
