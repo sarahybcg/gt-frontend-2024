3243f5d89ad51d98ed94ae014b5ca86d72a66920
sarahybcg <sarahybcg@gmail.com>
2025-01-28 23:48:55 -0400

    features/graphics-widgets-integration

---
+++ components/Widgets/widgets/widgets.vue
@@ -0,0 +1,16 @@
+<template src="./widgets.html">
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './widgets.js';
+
+export default defineComponent({
+  name: 'WidgetsComponent',
+});
+</script>
+
+<style src="./widgets.css">
+
+</style>
