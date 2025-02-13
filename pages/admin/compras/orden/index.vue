439a94c5d1f9301f9fb229d64f7db4cecfb46778
EdgarUWU <edgaraponte398@gmail.com>
2025-02-12 23:10:10 -0400

    formulario de ordenes de compras faltan arreglos

--- pages/admin/compras/orden/index.vue
+++ pages/admin/compras/orden/index.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="flex">
-        
-        <div class = "">
-            <SubNav module-name="Compras"/>
-        </div>
-        
-            <Datatableproduct></Datatableproduct>
-        
-        
-        </div>
-</template>
-<script setup>
-import Datatableproduct from '~/components/datatables/Datatableproduct/datatableproduct.vue';
-
-
-</script> 
+    <div class="p-6">
+      <h1 class="text-3xl font-bold mb-6">Gestión de Compras</h1>
+      <OrderForm />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import OrderForm from '~/components/forms/OrderForm/OrderForm.vue';
+  </script>
+  
+  <style scoped>
+  /* Puedes añadir estilos adicionales aquí */
+  </style>
+  
